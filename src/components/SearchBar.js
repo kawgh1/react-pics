aebb951aeb77bbbec05a216c035481c512312f75
thrwwy216 <thrwwy216@tutanota.com>
2021-08-13 11:54:07 -0500

    update

---
+++ src/components/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+
+render() {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+            </form>
+        </div>
+    
+    );
+}
+
+
+}
+
+export default SearchBar;
